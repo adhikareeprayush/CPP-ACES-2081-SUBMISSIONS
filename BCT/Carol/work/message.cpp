9684a137c96abeea1ab589296474ab288a6fa2b4
Carollama <lamacarol4@gmail.com>
2025-07-12 21:36:31 +0545

    Added my project and codes

---
+++ BCT/Carol/work/message.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void printMessage(string str ="Hello"){
+    cout<<str;
+}
+int main()
+{
+    printMessage("Goodmorning");
+ printMessage();
+    return 0;
+
+}
